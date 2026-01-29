c1d841ee9b786cb8163aaecfe5b4697b9554d80d
rkhademul8 <rkhademul8@gmail.com>
2026-01-29 16:23:14 +0600

    update

--- src/app/(site)/shop/_components/ProductCard.tsx
+++ src/app/(site)/shop/_components/ProductCard.tsx
@@ -30,25 +30,43 @@ const ProductCard: React.FC<ProductCardProps> = ({
       onClick={() => window.open(`/product/${source}-${id}`, "_blank")}
     >
       <Card className="product-card">
-        <CardMedia
-          alt={name}
-          component="img"
-          className="product-card-img"
-          image={fixImageUrl(image) || "/placeholder.png"}
-          onContextMenu={(e) => e.preventDefault()}
-        />
+        <Box>
+          <CardMedia
+            alt={name}
+            component="img"
+            className="product-card-img"
+            image={fixImageUrl(image) || "/placeholder.png"}
+            onContextMenu={(e) => e.preventDefault()}
+          />
+        </Box>
 
         <CardContent className="product-card-content">
-          <Box className="product-card-price-rating">
-            <span className="product-card-price">৳ {price}</span>
-            <span className="home-product-old-price">৳ 200</span>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box className="product-card-price-rating">
+              <span className="product-card-price">৳ {price}</span>
+              <span className="home-product-old-price">৳ 200</span>
+            </Box>
+            <Box>
+              <span className="product-card-sold">{soldCount || 0} SOLD</span>
+            </Box>
           </Box>
+
           <span className="product-card-title">{name}</span>
-          <span className="product-card-sold">{soldCount || 0} SOLD</span>
+
           <span className="product-card-deli">
             China to Bangladesh: 10-12 days
           </span>
         </CardContent>
+
+        <Box px={1} pb={2} pt={1}>
+          <button className="product-add-to-cart">Add to cart </button>
+        </Box>
       </Card>
     </Box>
   );
